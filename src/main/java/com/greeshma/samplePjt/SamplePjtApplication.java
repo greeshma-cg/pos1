0f33a71086ac691cf25a951c2fb5740161ea7274
greeshma <greeshmacg404@gmail.com>
2020-06-05 07:41:29 -0700

    completed

---
+++ src/main/java/com/greeshma/samplePjt/SamplePjtApplication.java
@@ -0,0 +1,13 @@
+package com.greeshma.samplePjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplePjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplePjtApplication.class, args);
+	}
+
+}
